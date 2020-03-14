a77a6bfcbae2e5f164731d7f4b13c162ae0d1525
ShippingTycoon <59520303+ShippingTycoon@users.noreply.github.com>
2020-03-14 14:00:44 +1300

    Update good-hash-function.c

--- code/7/sample-hash-functions/good-hash-function.c
+++ code/7/sample-hash-functions/good-hash-function.c
@@ -1,26 +1,140 @@
-/*
- * Most of the hash functions you'll find online will
- * be wrong, or ugly, or both! Here's a hash function that'll
- * just, you know, work!
- * Feel free to use this function, just provide attribution
- * in a comment!
- */
+// Implements a dictionary's functionality
 
-/**
- * A case-insensitive implementation of the djb2 hash function.
- * Change NUM_BUCKETS to whatever your number of buckets is.
- *
- * Adapted by Neel Mehta from
- * http://stackoverflow.com/questions/2571683/djb2-hash-function.
- */
-unsigned int hash_word(const char* word)
- {
-     unsigned long hash = 5381;
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <strings.h>
+#include <string.h>
 
-     for (const char* ptr = word; *ptr != '\0'; ptr++)
-     {
-         hash = ((hash << 5) + hash) + tolower(*ptr);
-     }
+#include "dictionary.h"
 
-     return hash % NUM_BUCKETS;
- }
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+unsigned int counter = 0;
+bool loaded = false;
+
+// Number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+    char *wordlower = malloc(LENGTH);
+
+    for (int i = 0; i < LENGTH; i++)
+    {
+        wordlower[i] = tolower(word[i]);
+    }
+
+    int index = hash(word);
+
+    node *tmp = table[index];
+
+    while (strcasecmp(wordlower, tmp->word) != 0)
+    {
+        if (tmp->next == NULL)
+        {
+            return false;
+        }
+
+        tmp = tmp->next;
+    }
+    return true;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    return (tolower(word[0]) - 97) % 25;
+
+}
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary)
+{
+    for (int i = 0; i < N; i++)
+    {
+        table[i] = NULL;
+    }
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+
+    char buffer[LENGTH + 1];
+
+    while (fscanf(file, "%s", buffer) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+
+        if (n == NULL)
+        {
+            return false;
+        }
+
+        strcpy(n->word, buffer);
+
+        n->next = NULL;
+
+        int index = hash(n->word);
+
+        //check if first of that letter to enter hash table
+        if (table[index] == NULL)
+        {
+            table[index] = n;
+        }
+
+        //otherwise place node at the start of the hash table
+        else
+        {
+            n->next = table[index];
+            table[index] = n;
+        }
+        counter++;
+    }
+    loaded = true;
+    fclose(file);
+    return true;
+}
+
+// Returns number of words in dictionary if loaded else 0 if not yet loaded
+unsigned int size(void)
+{
+    if (loaded == true)
+    {
+        return counter;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// Unloads dictionary from memory, returning true if successful else false
+bool unload(void)
+{
+    for (int j = 0; j <= 25; j++)
+    {
+        node *cursor = table[j];
+        while (cursor->next != NULL)
+        {
+            node *tmp = cursor->next;
+            free(cursor);
+            cursor = tmp;
+        }
+        free(cursor);
+    }
+
+    return true;
+}
